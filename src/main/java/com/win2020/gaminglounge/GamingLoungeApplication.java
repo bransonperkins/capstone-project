5795405e967d6190e96fd2b3c064012e2d21cb7f
bransonperkins <bransonperkins@gmail.com>
2020-08-12 16:49:20 -0400

    chat feature is working

--- src/main/java/com/win2020/gaminglounge/GamingLoungeApplication.java
+++ src/main/java/com/win2020/gaminglounge/GamingLoungeApplication.java
@@ -2,10 +2,14 @@ package com.win2020.gaminglounge;
 
 import com.win2020.gaminglounge.models.User;
 import com.win2020.gaminglounge.repositories.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GamingLoungeApplication implements CommandLineRunner {
@@ -22,5 +26,17 @@ public class GamingLoungeApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         this.userRepository.save(new User("mizew", "bransonperkins@gmail.com"));
         this.userRepository.save(new User("nelley", "noellebrinkley@gmail.com"));
+        this.userRepository.save(new User("carlton", "carlton@gmail.com"));
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+
 }
