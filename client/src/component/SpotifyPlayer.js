ac4cb01141bcd2c0fa29b394ed3749cb5f6e5a9b
David Juarez <djuar05@gmail.com>
2020-08-14 08:24:48 -0400

    adds imports for reactstrap input field

--- client/src/component/SpotifyPlayer.js
+++ client/src/component/SpotifyPlayer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Jumbotron } from 'reactstrap';
 import { Button, ButtonGroup } from 'reactstrap';
+import { InputGroup, Input } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 class SpotifyPlayer extends Component{
     constructor(props){
@@ -149,66 +150,49 @@ class SpotifyPlayer extends Component{
          } = this.state;
       
          return (
-          <div className="App container h-75">
-            <div>
-            <Jumbotron fluid className="bg-dark">
+          <div className="App container w-75">
+          <div className="">
             <h2>Listen to your Spotify playlist</h2>
-              <h2>Now Playing</h2>
-            </Jumbotron>
-
-              
-            </div>
+            
+          </div>
       
              {error && <p> {error}</p>} 
       
             {loggedIn ?
             (<div>
-              <Container className="themed-container">
-                <Row>
-                <Col xs="6" sm="4">
-                <img src= {albumCover} alt="Album Cover"/>
-                </Col>
-                <Col xs="6" sm="4">
-                <p>Artist: {artistName}</p>
-              <p>Track: {trackName}</p>
-              <p>Album: {albumName}</p>
-                </Col>
-                <Col sm="4">
-                <ButtonGroup vertical>
+              <h2>Now Playing</h2>
+              <Card className="">
+                <CardImg src= {albumCover}/>
+                <CardBody className="text-white bg-dark">
+                  <CardTitle>{trackName}</CardTitle>
+                  <CardSubtitle>By: {artistName}</CardSubtitle>
+                  <CardText>On Album: {albumName}</CardText>
+                </CardBody>
+                <ButtonGroup>
 
                 <Button color="info" onClick={() => this.onPrevClick()}>Previous</Button >
                 <Button color="danger" onClick={() => this.onPlayClick()}>{playing ? "Pause" : "Play"}</Button>
                 <Button color="warning" onClick={() => this.onNextClick()}>Next</Button>
-                  </ButtonGroup>
-                </Col>
-                </Row>
-              </Container>
-    
-              {/* <img src= {albumCover} />
-              <p>Artist: {artistName}</p>
-              <p>Track: {trackName}</p>
-              <p>Album: {albumName}</p>
-              <p>
-                <button  onClick={() => this.onPrevClick()}>Previous</button>
-                <button  onClick={() => this.onPlayClick()}>{playing ? "Pause" : "Play"}</button>
-                <button onClick={() => this.onNextClick()}>Next</button>
-              </p> */}
+                </ButtonGroup>
+              </Card>
             </div>)
             :
             (<div>
               <p className="App-intro">
                 Enter your Spotify access token. Get it from{" "}
-                
-                <a className="btn btn-success btn-link"href="https://beta.developer.spotify.com/documentation/web-playback-sdk/quick-start/#authenticating-with-spotify" target="blank">
-                 Spotify Web Playback SDK ACCESS TOKEN.
-                </a>
+                <Button color="success"href="https://beta.developer.spotify.com/documentation/web-playback-sdk/quick-start/#authenticating-with-spotify" target="blank">
+                  Spotify Web Player Access Token
+                </Button>
               </p>
-              <p>
-                <input type="text" value={token} onChange={e => this.setState({ token: e.target.value })} />
-              </p>
-              <p>
-                <button onClick={() => this.handleLogin()} >Go</button>
-              </p>
+              
+              <div>
+                <InputGroup size="sm"  >
+                  <Input type="text" value={token} onChange={e => this.setState({ token: e.target.value })} />
+                </InputGroup>
+              </div>
+              <div>
+                <Button color="success" onClick={() => this.handleLogin()} >Go</Button>
+              </div>
             </div>)
             }
           </div>
